6fcd96360fa8e54e5820c47815f7c31ff527f944
Poorvi <poorvip@argusoft.com>
2023-09-05 13:35:00 +0530

    practised more on junit

---
+++ practice_2/Main.java
@@ -0,0 +1,9 @@
+package practice_2;
+
+
+public class Main {
+    public static void main(String[] args) {
+       Utilities util = new Utilities();
+        System.out.println(util.removePairs("AABBCDEEF"));
+    }
+}
